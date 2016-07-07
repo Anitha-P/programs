38d1208f269034a1ba9328dede5b0aeb84e2634f
Anitha-P <anitha95anitha@gmail.com>
2016-07-07 21:02:49 +0530

    Create primeint.c

---
+++ primeint.c
@@ -0,0 +1,20 @@
+#include<stdio.h> #include<conio.h> void main() { int n,n1,i; 
+printf("\n enter thefirst number  :  "); 
+scanf("%d",&n);
+printf("\n enter the seond number  :  "); 
+scanf("%d",&n1);
+while(n<n1)
+{
+for(i=2;i<n;i++)
+{
+if(n%i==0)
+sum=0;
+else{
+sum=1;
+break;}
+
+if(sum==0)
+printf("%d",copy);
+}
+getch();
+}
