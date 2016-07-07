363c968dfc0192ab5c34150b6bb2bec23bf30f1d
Anitha-P <anitha95anitha@gmail.com>
2016-07-07 22:25:12 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio,h>
+void main()
+{
+int n,copy,sum;
+printf("\n enter the number  :  ");
+scanf("%d",&n);
+copy=n;
+while(n>0)
+{
+int r=n/10;
+sum=sum*10+r;
+n=n%10;
+}
+
+printf("the reverse of a number is : %d",sum);
+getch();
+}
