2a4b1c18846dfd298721caff37fbecd83779d2cf
Anitha-P <anitha95anitha@gmail.com>
2016-07-07 20:35:30 +0530

    Create countdigit.c

---
+++ countdigit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio,h>
+void main()
+{
+int n,count=0;
+printf("\n enter the number  :  ");
+scanf("%d",&n);
+while(n>0)
+{
+n=n/10;
+count++;
+}
+printf("\n no of digits are  : %d",count);
+getch();
+}
