358e58f5a5c9b60115b825a4ad3e7779a845f2fd
Anitha-P <anitha95anitha@gmail.com>
2016-07-08 06:23:04 +0530

    Create factofnum.c

---
+++ factofnum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+int fact=1;
+clrscr();
+printf("\n enter the number  :  ");
+scanf("%d",&n);
+for(int i=1;i<n;i++)
+fact=fact*i;
+printf("fact : %d",fact);
+getch();
+}
